e9b92e55517fbc71fca61f7480686e8b7729a79f
hsishejwvw6293519 <119119738+hsishejwvw6293519@users.noreply.github.com>
2022-11-26 22:13:00 +0600

    Create b.py

---
+++ b.py
@@ -0,0 +1,20 @@
+from telegram.ext import *
+
+
+
+def st(u,c):
+    u.message.reply_text("Welcome to this bot")
+
+def up(u,c):
+    f = str(c.bot.get_file(u.message.photo[-1].download()))
+    u.message.reply_text(f"Your File name is {f}")
+
+
+up = Updater("5609178227:AAFukFJwF229CI9T0SetibLZBHpHx0T7Qh0", use_context=True)
+
+up.dispatcher.add_handler(CommandHandler("start",st))
+up.dispatcher.add_handler(MessageHandler(Filters.photo,up))
+
+
+up.start_polling()
+up.idle()
