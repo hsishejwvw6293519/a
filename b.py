8a8116b5b7dcded2b83c93c3af4390ab972129fc
hsishejwvw6293519 <119119738+hsishejwvw6293519@users.noreply.github.com>
2022-11-26 22:24:49 +0600

    Update b.py

--- b.py
+++ b.py
@@ -5,7 +5,7 @@ from telegram.ext import *
 def st(u,c):
     u.message.reply_text("Welcome to this bot")
 
-def up(u,c):
+def upp(u,c):
     f = str(c.bot.get_file(u.message.photo[-1]).download())
     u.message.reply_text(f"Your File name is {f}")
 
@@ -13,7 +13,7 @@ def up(u,c):
 up = Updater("5609178227:AAFukFJwF229CI9T0SetibLZBHpHx0T7Qh0", use_context=True)
 
 up.dispatcher.add_handler(CommandHandler("start",st))
-up.dispatcher.add_handler(MessageHandler(Filters.photo,up))
+up.dispatcher.add_handler(MessageHandler(Filters.photo,upp))
 
 
 up.start_polling()
